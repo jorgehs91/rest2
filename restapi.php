78b224b3d0ad8db2a2ba00ec92164c0a24e0d68f
Jorge Henrique <jorgehs_lol@hotmail.com>
2018-11-19 21:50:57 -0200

    Implementado o metodo delete

--- restapi.php
+++ restapi.php
@@ -50,6 +50,14 @@
 // ];
 // $url = 'http://localhost/rest2/rest/GetAll';
 
+// $post = [
+//     'email' => 'jorge@teste.com',
+//     'pwd' => 'jorge',
+//     'table' => 'users',
+//     'conditions' => array('id' => 4)
+// ];
+// $url = 'http://localhost/rest2/rest/Delete';
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
