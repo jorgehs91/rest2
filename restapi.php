dd77407a976f952ffb199277e7c9803a8d1b591d
Jorge Henrique <jorgehs_lol@hotmail.com>
2018-11-19 22:01:26 -0200

    Adiçao de botoes

--- restapi.php
+++ restapi.php
@@ -1,11 +1,18 @@
 <?php
+$url = null;
 
+/* 
+*   LOGIN
+*/
 // $post = [
 //     'email' => 'teste@teste.com',
 //     'pwd' => 'teste'
 // ];
 // $url = 'http://localhost/rest2/rest/login';
 
+/* 
+*   INSERT
+*/
 // $post = [
 //     'email' => 'jorge@teste.com',
 //     'pwd' => 'jorge',
@@ -14,7 +21,9 @@
 // ];
 // $url = 'http://localhost/rest2/rest/Insert';
 
-
+/* 
+*   UPDATE
+*/
 // $post = [
 //     'email' => 'jorge@teste.com',
 //     'pwd' => 'jorge',
@@ -24,6 +33,9 @@
 // ];
 // $url = 'http://localhost/rest2/rest/Update';
 
+/* 
+*   GET ROW
+*/
 // $post = [
 //     'email' => 'jorge@teste.com',
 //     'pwd' => 'jorge',
@@ -32,16 +44,21 @@
 // ];
 // $url = 'http://localhost/rest2/rest/GetRow';
 
+/* 
+*   GET VALOR DO CAMPO
+*/
 // $post = [
 //     'email' => 'jorge@teste.com',
 //     'pwd' => 'jorge',
 //     'table' => 'users',
 //     'column_single' => 'user_fullname',
 //     'conditions' => array('id' => 17),
-
 // ];
 // $url = 'http://localhost/rest2/rest/GetSingleValue';
 
+/* 
+*   GET TODAS AS OCORRENCIAS ATRAVÉS DA CONDIÇÃO
+*/
 // $post = [
 //     'email' => 'jorge@teste.com',
 //     'pwd' => 'jorge',
@@ -50,6 +67,9 @@
 // ];
 // $url = 'http://localhost/rest2/rest/GetAll';
 
+/* 
+*   DELETE ROW ATRAVÉS DA CONDIÇÃO
+*/
 // $post = [
 //     'email' => 'jorge@teste.com',
 //     'pwd' => 'jorge',
@@ -58,11 +78,29 @@
 // ];
 // $url = 'http://localhost/rest2/rest/Delete';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-$response = curl_exec($ch); 
-
-var_export($response); 
 ?>
+
+<button class="insert">INSERT</button>
+<button class="update">UPDATE</button>
+<button class="getRow">GET ROW</button>
+<button class="getValue">GET VALUE</button>
+<button class="getAll">GET ALL</button>
+<button class="delete">DELETE</button>
+
+<?php
+if($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+    $response = curl_exec($ch); 
+    
+    var_export($response); 
+}
+?>
+
+<script>
+    function insert(){
+        
+    }
+</script>
